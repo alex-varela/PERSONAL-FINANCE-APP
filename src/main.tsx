60b10a3004978cfc638375d1c684189b52c46e3f
alex <alex@linkerfinance.com>
2025-07-17 23:02:55 -0600

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 // main.tsx
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
